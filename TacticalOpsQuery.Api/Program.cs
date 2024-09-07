a8cfa1958ee68c7f5b9aa56e2d68c40095c7c070
rdrgox <rdrgox@gmail.com>
2024-09-06 20:34:57 -0400

    add services program

--- TacticalOpsQuery.Api/Program.cs
+++ TacticalOpsQuery.Api/Program.cs
@@ -1,10 +1,13 @@
 using TacticalOpsQuery.Api.Endpoints;
+using TacticalOpsQuery.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IQueryUdpService, QueryUdpService>();
 
 var app = builder.Build();
 
